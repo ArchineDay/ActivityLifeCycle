37cbe3ed53f1dcc39a5c8c9e9cbfcd1c68b24ca4
OFFICE_ZOPUDT\daixiaoqi <2950715596@qq.com>
2023-12-25 09:01:43 +0800

    chronometer

--- app/src/main/java/com/example/activityjump/activity/ChronometerActivity.java
+++ app/src/main/java/com/example/activityjump/activity/ChronometerActivity.java
@@ -2,6 +2,7 @@ package com.example.activityjump.activity;
 
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -17,6 +18,8 @@ public class ChronometerActivity extends AppCompatActivity implements View.OnCli
     Chronometer chronometer;
     private long duration = 0;
 
+    private long liveSec = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,23 +33,56 @@ public class ChronometerActivity extends AppCompatActivity implements View.OnCli
         findViewById(R.id.reset).setOnClickListener(this);
 
 
+//        chronometer.setOnChronometerTickListener(chronometer -> {
+//            //设置倒计时
+//            if (liveSec > 0) {
+//                liveSec--;
+//                Log.d("liveSec", String.valueOf(liveSec));
+//            }
+//        });
+
+
     }
 
     @Override
     public void onClick(View v) {
 
         if (v.getId() == R.id.start) {
+            chronometer.setOnChronometerTickListener(chronometer -> {
+            });
             chronometer.setBase(SystemClock.elapsedRealtime());
+
+
+            chronometer.setOnChronometerTickListener(chronometer -> {
+                //设置倒计时
+                if (liveSec > 0) {
+                    liveSec--;
+                    Log.d("liveSecStart", String.valueOf(liveSec));
+                }
+            });
             chronometer.start();
+
         } else if (v.getId() == R.id.pause) {
             duration = SystemClock.elapsedRealtime() - chronometer.getBase();
             chronometer.stop();
         } else if (v.getId() == R.id.resume) {
+            chronometer.setOnChronometerTickListener(chronometer -> {
+            });
+
             chronometer.setBase(SystemClock.elapsedRealtime() - duration);
             chronometer.start();
+
+            chronometer.setOnChronometerTickListener(chronometer -> {
+                //设置倒计时
+                if (liveSec > 0) {
+                    liveSec--;
+                    Log.d("liveSecResume", String.valueOf(liveSec));
+                }
+            });
         } else if (v.getId() == R.id.reset) {
             chronometer.setBase(SystemClock.elapsedRealtime());
             chronometer.stop();
+            liveSec =2000;
         }
 
     }
